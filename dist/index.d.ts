3bec70f10583c401c696c0a1836c495c045e2ce1
Fabio Spampinato <spampinabio@gmail.com>
2018-11-04 18:50:39 +0100

    Ignored dist folder

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare class Activable {
-    _isActive: boolean;
-    constructor(active?: boolean);
-    isActive(): boolean;
-    activate(): this;
-    deactivate(): this;
-    toggleActive(active?: boolean): this;
-}
-export default Activable;
